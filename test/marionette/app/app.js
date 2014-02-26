evanxd/gaia

Bug 968692 - [settings] add marionette test for Screen lock panel
- add lockscreen related test

apps/settings/test/marionette/app/app.js
@@ -5,7 +5,8 @@ var Base = require('./base'),
     HotspotPanel = require('./regions/hotspot'),
     HotspotSettingsPanel = require('./regions/hotspot_settings'),
     SupportPanel = require('./regions/support'),
-    BatteryPanel = require('./regions/battery');
+    BatteryPanel = require('./regions/battery'),
+    ScreenLockPanel = require('./regions/screen_lock');
 
 // origin of the settings app
 var ORIGIN = 'app://settings.gaiamobile.org';
@@ -31,7 +32,8 @@ Settings.Selectors = {
   'hotspotPanel': '#hotspot',
   'hotspotSettingsTrigger': '#hotspot-settings-section button',
   'supportMenuItem': '#menuItem-help',
-  'batteryMenuItem': '#menuItem-battery'
+  'batteryMenuItem': '#menuItem-battery',
+  'screenLockMenuItem': '#menuItem-phoneLock'
 };
 
 Settings.prototype = {
@@ -78,6 +80,13 @@ Settings.prototype = {
     this._batteryPanel = this._batteryPanel ||
       new BatteryPanel(this.client);
     return this._batteryPanel;
+  },
+
+  get screenLockPanel() {
+    this.openPanel('screenLockMenuItem');
+    this._screenLockPanel = this._screenLockPanel ||
+      new ScreenLockPanel(this.client);
+    return this._screenLockPanel;
   },
 
   /**

a-os/settings

test/marionette/app/app.js
@@ -11,7 +11,9 @@ var Base = require('./base'),
     FeedbackPanel = require('./regions/feedback'),
     SoundPanel = require('./regions/sound'),
     NotificationsPanel = require('./regions/notifications'),
-    LanguagePanel = require('./regions/language');
+    LanguagePanel = require('./regions/language'),
+    NotificationsPanel = require('./regions/notifications'),
+    ScreenLockPanel = require('./regions/screen_lock');
 
 // origin of the settings app
 var ORIGIN = 'app://settings.gaiamobile.org';
@@ -43,7 +45,8 @@ Settings.Selectors = {
   'feedbackPanel': 'button[data-href="#improveBrowserOS-chooseFeedback"]',
   'soundMenuItem': '#menuItem-sound',
   'languagePanel': '#languages',
-  'languageMenuItem': '#menuItem-languageAndRegion'
+  'languageMenuItem': '#menuItem-languageAndRegion',
+  'screenLockMenuItem': '#menuItem-phoneLock'
 };
 
 Settings.prototype = {
@@ -111,6 +114,13 @@ Settings.prototype = {
     this._languagePanel = this._languagePanel ||
       new LanguagePanel(this.client);
     return this._languagePanel;
+  },
+
+  get screenLockPanel() {
+    this.openPanel('screenLockMenuItem');
+    this._screenLockPanel = this._screenLockPanel ||
+      new ScreenLockPanel(this.client);
+    return this._screenLockPanel;
   },
 
   /**